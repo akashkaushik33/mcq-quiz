f151dab772f4709757bb1b2f1c3b1e077fca1fc6
Akash Kaushik <akash.kaushik@indiqus.com>
2018-07-12 12:06:12 +0530

    production build fixes

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'mcq-quiz',
     proxyTable: {},
 
     // Various Dev Server settings
